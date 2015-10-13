e2ddefe98466b299071ba37208ccf7971e01772e
Neil Trini Lasrado <neil@frappe.io>
2015-10-13 11:48:25 +0530

    Fixed typo in Service Providers

--- community_erpnext_com/www/service_providers.py
+++ community_erpnext_com/www/service_providers.py
@@ -14,10 +14,10 @@ def get_context(context):
 	if frappe.form_dict.country:
 		condition += " and country=%s"
 		values.append(frappe.form_dict.country)
-		title = "ERPNext Service Provders in {0}".format(frappe.form_dict.country)
+		title = "ERPNext Service Providers in {0}".format(frappe.form_dict.country)
 	else:
 		condition += " and partner_category != 'Unverified'"
-		title = "ERPNext Service Provders"
+		title = "ERPNext Service Providers"
 
 	# if frappe.form_dict.service:
 	# 	condition += " and ifnull({0}, 0)=1".format(frappe.scrub(frappe.form_dict.service))
