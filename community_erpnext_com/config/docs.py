93fe349226da6807057978e4f79e85a7ec505ca1
Rushabh Mehta <rmehta@gmail.com>
2016-01-01 17:35:09 +0530

    [website] [cleanup]

--- community_erpnext_com/config/docs.py
+++ community_erpnext_com/config/docs.py
@@ -10,6 +10,8 @@ Add properties
 
 source_link = "https://github.com/frappe/community_erpnext_com"
 docs_base_url = "https://frappe.github.io/community_erpnext_com"
+headline = "Connects service seekers and providers"
+sub_heading = "ERPNext Community Portal allows users to post jobs, find service providers and post jobs"
 
 def get_context(context):
 	context.title = "ERPNext Community Portal"
--- community_erpnext_com/docs/contents.py
+++ community_erpnext_com/docs/contents.py
@@ -6,5 +6,4 @@ import frappe
 from frappe.website.utils import get_full_index
 
 def get_context(context):
-	context.full_index = get_full_index(base_url=getattr(frappe.local, "docs_base_url"),
-		extn = True)
+	context.full_index = get_full_index(extn = True)
